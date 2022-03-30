7ea775dd6bee0d8f62242dbc5cc1b14d2b4d6057
davidnugent2425 <david.nugent2425@gmail.com>
2022-03-30 07:20:21 +0100

    demo

--- src/cc_he_server.py
+++ src/cc_he_server.py
@@ -5,40 +5,12 @@ import torch
 import pickle
 import xgboost as xgb
 from ppxgboost import PPBooster as ppbooster
-from train_nn import Classifier
+from train_nn import Classifier, HEModel
 from IPython.display import display
 from datetime import datetime
 from cc_he_utils import json_serialize, json_deserialize
 from train_utils import show_time_taken
 
-# HE Model
-class HEModel:
-    def __init__(self, hidden_layer, output_layer):
-        self.hidden_layer_weight = hidden_layer.weight.t().tolist()
-        self.hidden_layer_bias = hidden_layer.bias.tolist()
-        self.output_layer_weight = output_layer.weight.t().tolist()
-        self.output_layer_bias = output_layer.bias.tolist()
-        
-    def forward(self, enc_x, show=False, plaintext=False):
-        self.show = show
-        self.plaintext = plaintext
-        self.debug_output('Input', enc_x)
-        enc_y = enc_x.mm(self.hidden_layer_weight) + self.hidden_layer_bias
-        self.debug_output('Hidden Layer', enc_y)
-        enc_y *= enc_y
-        self.debug_output('Activation', enc_y)
-        enc_y = enc_y.mm(self.output_layer_weight) + self.output_layer_bias
-        self.debug_output('Output Layer', enc_y)
-        return enc_y
-    
-    def debug_output(self, msg, vec):
-        if self.show:
-            print(msg)
-            if self.plaintext: vec = torch.tensor(vec.decrypt())
-            print(vec)
-    
-    def __call__(self, enc_x, show=False, plaintext=False):
-        return self.forward(enc_x, show, plaintext)
 
 server_files_path = './server-files/'
 ulb_plaintext_nn_model = pickle.load(open(server_files_path+'ulb-nn.pt', "rb"))
--- src/train_nn.py
+++ src/train_nn.py
@@ -7,7 +7,67 @@ from functools import partial
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from datetime import datetime
-from train_utils import load_dataset, train_test_split_undersample, report_metrics, nn_configs, prep_data_nn, create_dataloader, parse_training_args, evaluate_predictions, show_time_taken
+import tenseal as ts
+from train_utils import load_dataset, train_test_split_undersample, report_metrics, nn_configs, prep_data_nn, create_dataloader, parse_training_args, evaluate_predictions, show_time_taken, convert_to_binary
+from cc_he_utils import setup_tenseal_context
+
+def test_encrypted_nn(plaintext_model, xtest, ytest, scaler):
+    print('\nTesting encrypted model...')
+    start_time = datetime.now()
+    start_index = ytest.index[0]
+    test_idxs = np.array(ytest[ytest==1].index)
+    test_idxs = np.random.choice(test_idxs, 75, replace = False) - start_index
+    xtest = xtest.iloc[test_idxs, :]
+    ytest = ytest.iloc[test_idxs]
+    xtest, ytest = prep_data_nn(xtest, ytest, scaler)
+
+    print('Making plaintext predictions.')
+    preds_plaintext = convert_to_binary(plaintext_model(torch.tensor(xtest).float()).detach().numpy())
+    print('Plaintext predictions:', preds_plaintext)
+
+    context = setup_tenseal_context()
+    encrypted_model = HEModel(plaintext_model.hidden_layer, plaintext_model.output_layer)
+    print('Encrypting inputs, making encrypted predictions, and decrypting.')
+    preds_decrypted = []
+    for row in xtest:
+        encrypted_input = ts.ckks_vector(context, row)
+        encrypted_pred = encrypted_model(encrypted_input)
+        decrypted_pred = encrypted_pred.decrypt()
+        preds_decrypted.append(decrypted_pred)
+    preds_decrypted = convert_to_binary(np.array(preds_decrypted).squeeze())
+    print('Decrypted predictions:', preds_decrypted)
+
+    result = np.array_equal(preds_plaintext, preds_decrypted)
+    print('Success!') if result else print('Failed.')
+
+# HE Model
+class HEModel:
+    def __init__(self, hidden_layer, output_layer):
+        self.hidden_layer_weight = hidden_layer.weight.t().tolist()
+        self.hidden_layer_bias = hidden_layer.bias.tolist()
+        self.output_layer_weight = output_layer.weight.t().tolist()
+        self.output_layer_bias = output_layer.bias.tolist()
+        
+    def forward(self, enc_x, show=False, plaintext=False):
+        self.show = show
+        self.plaintext = plaintext
+        self.debug_output('Input', enc_x)
+        enc_y = enc_x.mm(self.hidden_layer_weight) + self.hidden_layer_bias
+        self.debug_output('Hidden Layer', enc_y)
+        enc_y *= enc_y
+        self.debug_output('Activation', enc_y)
+        enc_y = enc_y.mm(self.output_layer_weight) + self.output_layer_bias
+        self.debug_output('Output Layer', enc_y)
+        return enc_y
+    
+    def debug_output(self, msg, vec):
+        if self.show:
+            print(msg)
+            if self.plaintext: vec = torch.tensor(vec.decrypt())
+            print(vec)
+    
+    def __call__(self, enc_x, show=False, plaintext=False):
+        return self.forward(enc_x, show, plaintext)
 
 # Plaintext Model
 class Classifier(torch.nn.Module):
@@ -51,7 +111,7 @@ def train(x, y, configs, project_name, wandb_mode=None):
     config = wandb.config
 
     # split data into data used for training and data used for testing
-    xtrain, xtest, ytrain, ytest = train_test_split(x, y, test_size=0.2, shuffle=False)
+    xtrain, xtest_df, ytrain, ytest_df = train_test_split(x, y, test_size=0.2, shuffle=False)
     # split training data into training and validation sets for hyperparameter tuning
     xtrain, xvalid, ytrain, yvalid = train_test_split_undersample(xtrain, ytrain, config.undersampling_num_negatives, test_size=0.1875)
 
@@ -60,7 +120,7 @@ def train(x, y, configs, project_name, wandb_mode=None):
 
     xtrain, ytrain = prep_data_nn(xtrain, ytrain, scaler)
     xvalid, yvalid = prep_data_nn(xvalid, yvalid, scaler)
-    xtest, ytest = prep_data_nn(xtest, ytest, scaler)
+    xtest, ytest = prep_data_nn(xtest_df, ytest_df, scaler)
 
     train_dl = create_dataloader(xtrain, ytrain)
     valid_dl = create_dataloader(xvalid, yvalid)
@@ -90,6 +150,8 @@ def train(x, y, configs, project_name, wandb_mode=None):
     report_metrics(preds, yvalid)
 
     show_time_taken('Time taken to train model:', start_time, datetime.now())
+
+    test_encrypted_nn(model, xtest_df, ytest_df, scaler)
 
     test_preds = model(torch.tensor(xtest).float()).detach().numpy()
     evaluate_predictions(test_preds, ytest)
